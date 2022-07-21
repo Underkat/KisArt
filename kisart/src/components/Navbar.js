51b114ac9a4598780242df3fdcfc310d7b7f3927
Maxim Churzin <mchurzin@outlook.com>
2022-07-20 23:17:47 -0400

    Additional components
    Started working on the Navbar and added Routes

---
+++ kisart/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <link to="/" className="navbar-logo">
+            KIANA STANEK
+          </link>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
