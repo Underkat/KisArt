3ec34e3018bbec912b769ecc82d783312b800008
Maxim Churzin <alienz280@gmail.com>
2022-07-21 11:20:17 -0400

    Navbar
    Added navbar (styling + routes)

--- kisart/src/App.js
+++ kisart/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
